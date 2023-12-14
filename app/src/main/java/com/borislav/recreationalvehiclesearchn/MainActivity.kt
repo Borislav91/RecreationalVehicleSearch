cbbe3b0bcded92db4b0d17323b6300ee920937b4
Borislav91 <bobby.velchev@gmail.com>
2023-12-14 21:28:55 +0200

    Remove unused code

--- app/src/main/java/com/borislav/recreationalvehiclesearchn/MainActivity.kt
+++ app/src/main/java/com/borislav/recreationalvehiclesearchn/MainActivity.kt
@@ -3,12 +3,6 @@ package com.borislav.recreationalvehiclesearchn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.borislav.recreationalvehiclesearchn.ui.theme.RecreationalVehicleSearchTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,18 +12,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecreationalVehicleSearchTheme {
-        Greeting("Android")
-    }
-}
