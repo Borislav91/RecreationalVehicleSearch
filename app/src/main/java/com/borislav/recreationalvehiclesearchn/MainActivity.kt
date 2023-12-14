325e69f0b1793b64a41f3b5fa4c0adae8a944c26
Borislav91 <bobby.velchev@gmail.com>
2023-12-14 21:26:04 +0200

    Add gradle dependencies, refactor MainActivity

--- app/src/main/java/com/borislav/recreationalvehiclesearchn/MainActivity.kt
+++ app/src/main/java/com/borislav/recreationalvehiclesearchn/MainActivity.kt
@@ -3,9 +3,6 @@ package com.borislav.recreationalvehiclesearchn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,15 +13,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RecreationalVehicleSearchTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
 }
