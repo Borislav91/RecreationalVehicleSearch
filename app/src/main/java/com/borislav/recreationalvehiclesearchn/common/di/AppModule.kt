15ee7a1de52a36f4eae26efc2b532d8f66017997
Borislav91 <bobby.velchev@gmail.com>
2023-12-14 21:51:33 +0200

    Add DI and UseCase in domain layer

--- app/src/main/java/com/borislav/recreationalvehiclesearchn/common/di/AppModule.kt
+++ app/src/main/java/com/borislav/recreationalvehiclesearchn/common/di/AppModule.kt
@@ -1,10 +1,52 @@
 package com.borislav.recreationalvehiclesearchn.common.di
 
+import com.borislav.recreationalvehiclesearchn.data.repository.VehicleListingRepositoryImpl
+import com.borislav.recreationalvehiclesearchn.domain.repository.OutdoorsyApiService
+import com.borislav.recreationalvehiclesearchn.domain.repository.VehicleListingRepository
+import com.borislav.recreationalvehiclesearchn.domain.usecase.SearchRentalListingsUseCase
+import com.google.gson.Gson
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class AppModule {
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: Gson): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://search.outdoorsy.com/rentals/") // Base URL for the Outdoorsy API
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson {
+        return Gson()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOutdoorsyApiService(retrofit: Retrofit): OutdoorsyApiService {
+        return retrofit.create(OutdoorsyApiService::class.java)
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideVehicleListingRepository(apiService: OutdoorsyApiService): VehicleListingRepository {
+        return VehicleListingRepositoryImpl(apiService)
+    }
+
+    @Singleton
+    @Provides
+    fun provideSearchRentalListingsUseCase(vehicleListingRepository: VehicleListingRepository): SearchRentalListingsUseCase {
+        return SearchRentalListingsUseCase(vehicleListingRepository)
+    }
 }
