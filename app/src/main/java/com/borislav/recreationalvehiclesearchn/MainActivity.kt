419481b644b18a76eb5cfc3aa9b5c26ebf713d6f
Borislav91 <bobby.velchev@gmail.com>
2023-12-14 22:50:20 +0200

    Refactor Manifest add OutdoorsyListingsScreen() to MainActivity

--- app/src/main/java/com/borislav/recreationalvehiclesearchn/MainActivity.kt
+++ app/src/main/java/com/borislav/recreationalvehiclesearchn/MainActivity.kt
@@ -3,11 +3,24 @@ package com.borislav.recreationalvehiclesearchn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.borislav.recreationalvehiclesearchn.ui.OutdoorsyListingsScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                OutdoorsyListingsScreen()
+            }
         }
     }
 }
